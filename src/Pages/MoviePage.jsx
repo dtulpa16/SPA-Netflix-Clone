71bf34aee66b43f991a6161bd95785828fe101b6
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-16 12:50:10 -0500

    Added Axios Request To Fetch Movies

--- src/Pages/MoviePage.jsx
+++ src/Pages/MoviePage.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
 
 export default function MoviePage() {
-  return (
-    <div>MoviePage</div>
-  )
+  const fetchMovies = async (searchTerm = "The Hobbit") => {
+    try {
+      let response = await axios.get(
+        `https://online-movie-database.p.rapidapi.com/title/v2/find?title=${searchTerm}`,
+        {
+          headers: {
+            "X-RapidAPI-Key": process.env.REACT_APP_MOVIE_API_KEY,
+            "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+          },
+        }
+      );
+      console.log("Movie API response: ", response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  
+  return <div>MoviePage</div>;
 }
