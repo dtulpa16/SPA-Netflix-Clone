715a19811903f213f7210eb3dd1e565072bea536
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-17 16:05:00 -0500

    Adjusted padding Between Movie Thumbnails

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 
 export default function MovieCard({ singleMovie }) {
   const [hoverToggle, setHoverToggle] = useState(false);
-  return (
+   return singleMovie.image?.url ? (
     <div
       onMouseEnter={() => setHoverToggle(true)}
       onMouseLeave={() => setHoverToggle(false)}
       class={`trasition duration-200 ease-in-out hover:scale-105 min-h-[280px] bg-black `}
     >
-      <img
-        src={singleMovie?.image?.url}
+      {<img
+        src={singleMovie.image.url}
         width="250"
         className={`h-[350px] ${hoverToggle ? "opacity-50 drop-shadow-sm shadow-sm shadow-gray-100" : ""}`}
-      />
+      />}
       {hoverToggle === true ? (
         <h3 class="text-white fixed pl-2 text-md bottom-0 font-bold pb-2">
           {singleMovie.title}
         </h3>
       ) : null}
-    </div>
-  );
+    </div> 
+  ): null
 }
--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 
 export default function MovieList({ movies }) {
   return (
-    <div className="flex flex-wrap gap-8 justify-center p-10">
+    <div className="flex flex-wrap gap-10 justify-center p-10">
       {movies.map((el) => (
         <MovieCard singleMovie={el} />
       ))}
