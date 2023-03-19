14d39e121e6826b27e8e8afdbc891ae078924429
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-19 17:27:36 -0500

    Added Movie Details Modal

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MovieDetailsModal from "./MovieDetailsModal";
 /**
  * Renders a single movie or TV show card.
  * @param {Object} props - Component props
@@ -7,23 +8,44 @@ import React, { useState } from "react";
  */
 export default function MovieCard({ singleMovie }) {
   const [hoverToggle, setHoverToggle] = useState(false);
-  // const [hoverToggle, setHoverToggle] = useState(false);
-   return singleMovie.image?.url ? (
-    <div
-      onMouseEnter={() => setHoverToggle(true)}
-      onMouseLeave={() => setHoverToggle(false)}
-      class={`trasition duration-200 ease-in-out hover:scale-105 min-h-[280px] bg-black `}
-    >
-      {<img
-        src={singleMovie.image.url}
-        width="250"
-        className={`h-[350px] ${hoverToggle ? "opacity-50 drop-shadow-sm shadow-sm shadow-gray-100" : ""}`}
-      />}
-      {hoverToggle === true ? (
-        <h3 class="text-white fixed pl-2 text-md bottom-0 font-bold pb-2">
-          {singleMovie.title}
-        </h3>
-      ) : null}
-    </div> 
-  ): null
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [movieId, setMovieId] = useState();
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  const handleClick = (movieId) => {
+    setMovieId(movieId.id.replace("/title/", "").replace("/", ""));
+    openModal();
+  };
+  return singleMovie.image?.url ? (
+    <div>
+      <div
+        onClick={() => {
+          handleClick(singleMovie);
+        }}
+        onMouseEnter={() => setHoverToggle(true)}
+        onMouseLeave={() => setHoverToggle(false)}
+        class={`cursor-pointer trasition duration-200 ease-in-out hover:scale-105 min-h-[280px] bg-black `}
+      >
+        {
+          <img
+            src={singleMovie.image.url}
+            width="250"
+            className={`h-[350px] ${hoverToggle ? "opacity-50" : ""}`}
+          />
+        }
+        {hoverToggle === true ? (
+          <h3 class="text-white fixed pl-2 text-md bottom-0 font-bold pb-2">
+            {singleMovie.title}
+          </h3>
+        ) : null}
+      </div>
+      {isModalOpen && (
+        <MovieDetailsModal
+          movieId={movieId}
+          isModalOpen={isModalOpen}
+          closeModal={closeModal}
+        />
+      )}
+    </div>
+  ) : null;
 }
