34fcd4ead322ad07a0dcd4df2cec692300c2ac37
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-21 10:08:40 -0500

    Moved Login Component To Authentication Folder

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { auth, provider } from '../firebaseConfig';
+import { auth, provider } from '../../firebaseConfig';
 
 const Login = () => {
   const signInWithGoogle = async () => {
