f36f32029e4dbfec2d62333625ece6dfb6f64ffa
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-21 20:48:42 -0500

    Added YT Key To .env File

--- src/Components/MovieDetailsModal.jsx
+++ src/Components/MovieDetailsModal.jsx
@@ -53,7 +53,7 @@ export default function MovieDetailsModal({
   const fetchTrailer = async (movie) => {
     try {
       const response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/search?q=${movie.title.title} trailer&key=${KEY}&part=snippet&type=video&maxResults=6`
+        `https://www.googleapis.com/youtube/v3/search?q=${movie.title.title} trailer&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&part=snippet&type=video&maxResults=6`
       );
       debugger;
       setTrailerKey(response.data.items[0].id.videoId);
