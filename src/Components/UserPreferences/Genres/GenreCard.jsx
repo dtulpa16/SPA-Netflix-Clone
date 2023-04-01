53b9638f1f665ce79a77e12aec801ed49fd9b902
Daniel Tulpa <dtulpa16@yahoo.com>
2023-04-01 16:22:41 -0600

    Added + Icon From Heroicons To Genre Choices

--- src/Components/UserPreferences/Genres/GenreCard.jsx
+++ src/Components/UserPreferences/Genres/GenreCard.jsx
@@ -1,13 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-export default function GenreCard({ genre,addToSelected }) {
+export default function GenreCard({ genre, addToSelected }) {
   const [active, setActive] = useState(false);
-  
+
   return (
     <div
       className=" bg-zinc-300 rounded-md h-min flex flex-row items-center"
       onClick={() => addToSelected(genre)}
     >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-4 h-4"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 4.5v15m7.5-7.5h-15"
+          stroke="currentColor"
+          className="text-black"
+        />
+      </svg>
+
       <h1 className="text-black text-lg">{genre.replace("-", " ")}</h1>
     </div>
   );
